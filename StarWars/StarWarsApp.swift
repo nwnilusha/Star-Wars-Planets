99c64348f5c71b7fdf76b0dcbb5b86a9d2b4351c
Nilusha Wimalasena <nilusha.wimalasena@trustpayments.com>
2022-09-17 16:24:09 +0530

    Initial Commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Nilusha Wimalasena on 2022-09-17.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
